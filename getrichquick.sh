442440277df33e33f0e4f88376da398df1b97b5f
Estefan Hu <ehu@goodwaygroup.com>
2023-03-08 23:12:09 -0800

    chore(r): Testing R

---
+++ getrichquick.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "What is your name?"
+
+read name
+
+echo "What is your age?"
+
+read age
+
+echo "Hello $name, you are $age years old"
+
+echo $USER
+echo $PWD, $SHELL
---
+++ robyn.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Hello world"
+
+which Rscript
+
+Rscript first.R
